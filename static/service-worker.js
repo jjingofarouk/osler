602ba8561554900c41899587d4e57683cfd90b31
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-21 04:21:17 +0300

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,15 @@
+const CACHE_NAME = 'jingo-mentor-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/bootstrap.min.css',
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('osler-v1').then(cache => {
-      return cache.addAll(['/', '/static/css/styles.css', '/static/js/main.js']);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
@@ -10,6 +18,21 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });

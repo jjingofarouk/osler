8ce054239a2f2f0cc6408b0d54e567d315031f03
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-25 16:17:03 +0300

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,11 @@
-// static/service-worker.js
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('dr-jingo-cache').then((cache) => {
             return cache.addAll([
                 '/',
+                '/chat',
+                '/case_study',
+                '/history',
                 '/static/manifest.json',
                 '/static/icons/icon-192.png',
                 '/static/assets/img/bothead.png'
